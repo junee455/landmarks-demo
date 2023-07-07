76a45d3345bb7afa94f8f75f87aee3c440819432
Michael Maslyanskiy <michaelma455@gmail.com>
2023-07-07 09:29:10 +0300

    added script and style files

---
+++ index.js
@@ -0,0 +1,27 @@
+async function sleep(time) {
+  await new Promise((resolve) => {
+    setTimeout(() => resolve(), time);
+  })
+}
+
+async function main() {
+
+  await sleep(5000);
+
+  try {
+    const conf = JSON.parse(window.javascript_obj?.getCameraHardwareConfigString());
+    console.log("intrinsics", conf);
+  } catch {
+    console.log("failed to get intrinsics");
+  }
+
+  for (; ;) {
+    try {
+      const slam = JSON.parse(window.javascript_obj?.getMetaDataString())
+      console.log("slam", slam)
+    } catch {
+      console.log("failed to get slam");
+    }
+    sleep(3000);
+  }
+}
